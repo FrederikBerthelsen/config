ee827d53941ca420c81fc36e0e8bc89cd29cc4d6
FrederikBerthelsen <frede2501@hotmail.com>
2020-02-09 10:48:06 +0100

    Updated config to include spotify

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,10 @@
 import XMonad
+import XMonad.ManageHook
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig
+import XMonad.Actions.SpawnOn
 
 main :: IO ()
 main = do 
@@ -9,10 +12,15 @@ main = do
 
 xmobarShowHideKeymap (XConfig {modMask = modKey}) = (modKey, xK_b)
 
+myWorkspaces = [ "1","2","3","4","5","6","7","8","9:spotify" ]
+
+myManageHook = composeAll
+   [ className =? "" --> doShift "9:spotify" ]
+
 myStartupHook :: X ()
-myStartupHook = 
-    spawn
-        "compton --backend glx --xrender-sync --xrender-sync-fence -fcCz -l -17 -t -17 -D 5"
+myStartupHook = do
+    spawn "compton --backend glx --xrender-sync --xrender-sync-fence -fcCz -l -17 -t -17 -D 5"
+    spawnOn "9:spotify" "spotify"
 
 myConfig = def
     { modMask            = mod4Mask
@@ -22,6 +30,8 @@ myConfig = def
     , focusedBorderColor = "#58E894" 
     , focusFollowsMouse  = False 
     , startupHook        = myStartupHook
+    , manageHook         = manageDocks <+> myManageHook
+    , workspaces         = myWorkspaces
     }
     `additionalKeysP` myKeys
 
@@ -30,4 +40,8 @@ myKeys =
     [ ("<XF86AudioRaiseVolume>", spawn "amixer -D pulse sset Master 5%+")
     , ("<XF86AudioLowerVolume>", spawn "amixer -D pulse sset Master 5%-")
     , ("<XF86AudioMute>", spawn "amixer -D pulse set Master 1+ toggle")
+    , ("<XF86AudioPlay>", spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
+    , ("<XF86AudioStop>", spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop")
+    , ("<XF86AudioNext>", spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
+    , ("<XF86AudioPrev>", spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
     ]
